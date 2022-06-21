523851f062713a53db4fcb42818a137956b9d88c
schelen01 <schelengrossel@gmail.com>
2022-06-20 22:52:14 -0300

    Primeiro commit

---
+++ src/Conta.java
@@ -0,0 +1,9 @@
+public class Conta {
+//especificação em Java
+       double saldo;
+       int agencia = 42; //especifiquei um valor padrão para agência
+       int numero;
+       String titular;
+}
+
+//instanciar é criar
---
+++ src/CriaConta.java
@@ -0,0 +1,30 @@
+import java.io.PrintStream;
+
+public class CriaConta {
+    public static void main(String[] args) { //ctrl espaço enter
+        Conta primeiraConta = new Conta(); //o primeiro "Conta" é o tipo da variável "primeiraConta"
+        primeiraConta.saldo =200; // atribui o valor para o atributo
+        System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta =new Conta(); // criou um espaço de memória para a conta
+        segundaConta.saldo =50;
+
+        //precisamos orientar ao objeto, poi só "saldo =50" o Java não entende de qual conta é
+
+        System.out.println("primeira conta tem " + primeiraConta.saldo);
+        System.out.println("segunda conta tem " + segundaConta.saldo);
+
+
+        //valor padrão para o JAVA é default, ou zero
+        System.out.println(primeiraConta.numero);
+
+        //Agora estou redefinindo um valor para agencia para segundaConta
+        segundaConta.agencia = 146;
+        System.out.println("Agora a segunda conta esta " + "na agencia " + segundaConta.agencia);
+
+    }
+
+}
